6a77385bf1dbc743d17e0be2366a47212fe4e6e9
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2025-04-25 13:47:40 +0100

    add electrolyte concentration

--- p2d.cpp
+++ p2d.cpp
@@ -100,6 +100,14 @@ class ParticleConcentrationOperator : public EquationOperator
       virtual ~ParticleConcentrationOperator();
 };
 
+class ElectrolyteConcentrationOperator : public EquationOperator
+{
+   public:
+      using EquationOperator::EquationOperator;
+      virtual void SetParameters(const Vector &u);
+      virtual ~ElectrolyteConcentrationOperator();
+};
+
 int main(int argc, char *argv[])
 {
    // 1. Initialize MPI and HYPRE.
@@ -391,6 +399,54 @@ void ParticleConcentrationOperator::SetParameters(const Vector &u)
 }
 
 ParticleConcentrationOperator::~ParticleConcentrationOperator()
+{
+   delete C;
+   delete M;
+   delete K;
+   delete Q;
+}
+
+
+void ElectrolyteConcentrationOperator::SetParameters(const Vector &u)
+{
+   ParGridFunction u_gf(&fespace);
+   u_gf.SetFromTrueDofs(u);
+
+   real_t a, tplus;
+   real_t temp = (1 - tplus) * a; 
+
+   IntegrationRule ir = IntRules.Get(Geometry::SEGMENT, 6);
+
+   //std::cout << "Number of points " << ir.GetNPoints() << std::endl;
+
+   ConstantCoefficient one(1.0);
+   ConstantCoefficient nbcCoef(1.0);
+   ConstantCoefficient Coeff(temp);
+
+   delete M;
+   M = new ParBilinearForm(&fespace);
+   M->AddDomainIntegrator(new MassIntegrator(one, &ir));
+   M->Assemble(0); // keep sparsity pattern of M and K the same
+   M->FormSystemMatrix(ess_tdof_list, Mmat);
+
+   delete K;
+   K = new ParBilinearForm(&fespace);
+   K->AddDomainIntegrator(new DiffusionIntegrator(one, &ir));
+   K->Assemble(0); // keep sparsity pattern of M and K the same
+   K->FormSystemMatrix(ess_tdof_list, Kmat);
+
+   delete Q;
+   Q = new ParLinearForm(&fespace);
+   Q->AddDomainIntegrator(new DomainLFIntegrator(Coeff));
+   Q->Assemble();
+   Qvec = std::move(*(Q->ParallelAssemble()));
+   Qvec.SetSubVector(ess_tdof_list, 0.0); // do we need this?
+   
+   delete C;
+   C = NULL; // re-compute C on the next ImplicitSolve
+}
+
+ElectrolyteConcentrationOperator::~ElectrolyteConcentrationOperator()
 {
    delete C;
    delete M;
