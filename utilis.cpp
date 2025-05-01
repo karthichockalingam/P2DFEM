ee0ab06f30c16d7006e69d81a61f15655695e5e3
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2025-05-01 23:31:40 +0100

    refractor FluxJGridFuncCoefficient

--- utilis.cpp
+++ utilis.cpp
@@ -1,28 +1,45 @@
 
 #include "utilis.hpp"
 
-double  function3(const double & electrolyte_potential, const double & electrode_potential, const Vector & x)
+const real_t m = 1.0;
+const real_t cmax = 1.0;
+const real_t T = 1.0;
+
+double  FluxJExt(const double & ce, const double & cs)
 { 
-   double val = (electrode_potential - electrolyte_potential) / (2.0);
+   return m * pow(((cs - cmax) / (cs * ce)), 0.5);
+}
+
+double  FluxJ(const double & electrolyte_potential, const double & electrode_potential, const double & electrolyte_concentration, const double & electrode_surface_concentration, const Vector & x)
+{ 
+   double val = 2.0 * FluxJExt(electrolyte_concentration, electrode_surface_concentration) * (electrode_potential - electrolyte_potential) / (T);
 
    return std::sinh(val);
 }
 
-double  function4(const double & ce, const double & cs, const double & cmax, const Vector & x)
-{ 
-   return pow(((cs - cmax) / (cs * ce)), 0.5);
-}
-
-FluxJGridFuncCoefficient::FluxJGridFuncCoefficient(const GridFunction & electrolyte_potential, const GridFunction & electrode_potential
-    , function<double(const double &, const double &, const Vector &)> foo)
-    : _electrolyte_potential(electrolyte_potential), _electrode_potential(electrode_potential), 
+FluxJGridFuncCoefficient::FluxJGridFuncCoefficient(
+    const GridFunction & electrolyte_potential, 
+    const GridFunction & electrode_potential,
+    const GridFunction & electrolyte_concentration, 
+    const double & electrode_surface_concentration, 
+    Func foo): 
+    _electrolyte_potential(electrolyte_potential), 
+    _electrode_potential(electrode_potential), 
+    _electrolyte_concentration(electrolyte_concentration),
+    _electrode_surface_concentration(electrode_surface_concentration),
     GFunction( move(foo) ) {};
 
-FluxJGridFuncCoefficient::FluxJGridFuncCoefficient(const GridFunction & electrolyte_potential, const GridFunction & electrode_potential
-    , function<double(const double &, const double &, const Vector &, const double)> foo)
-    : _electrolyte_potential(electrolyte_potential), _electrode_potential(electrode_potential),
+FluxJGridFuncCoefficient::FluxJGridFuncCoefficient(
+    const GridFunction & electrolyte_potential, 
+    const GridFunction & electrode_potential,
+    const GridFunction & electrolyte_concentration,
+    const double & electrode_surface_concentration,   
+    FuncT foo): 
+    _electrolyte_potential(electrolyte_potential), 
+    _electrode_potential(electrode_potential),
+    _electrolyte_concentration(electrolyte_concentration),
+    _electrode_surface_concentration(electrode_surface_concentration),
     TDGFunction( move(foo) ) {};
-
 
 double 
 FluxJGridFuncCoefficient::Eval(ElementTransformation &T, const IntegrationPoint &ip)
@@ -30,30 +47,6 @@ FluxJGridFuncCoefficient::Eval(ElementTransformation &T, const IntegrationPoint 
         double x[3];
         Vector transip(x, 3);
         T.Transform(ip, transip);
-        return (GFunction)? GFunction(_electrolyte_potential.GetValue(T, ip), _electrode_potential.GetValue(T, ip), transip) 
-            : TDGFunction(_electrolyte_potential.GetValue(T, ip), _electrode_potential.GetValue(T, ip), transip, GetTime() );
-    }
-
-FluxJExtGridFuncCoefficient::FluxJExtGridFuncCoefficient(const GridFunction & electrolyte_concentration, const double & electrode_surface_concentration,
-        const double & electrode_max_concentration, function<double(const double &, const double &, const double &, const Vector &)> foo)
-        : _electrolyte_concentration(electrolyte_concentration), 
-        _electrode_surface_concentration(electrode_surface_concentration),
-        _electrode_max_concentration(electrode_max_concentration),
-        GFunction( move(foo) ) {};
- 
-FluxJExtGridFuncCoefficient::FluxJExtGridFuncCoefficient(GridFunction & electrolyte_concentration, const double & electrode_surface_concentration,
-        const double & electrode_max_concentration, function<double(const double &, const double &, const double &, const Vector &, const double)> foo)
-        : _electrolyte_concentration(electrolyte_concentration), 
-        _electrode_surface_concentration(electrode_surface_concentration),
-        _electrode_max_concentration(electrode_max_concentration),
-        TDGFunction( move(foo) ) {};
- 
-double 
-FluxJExtGridFuncCoefficient::Eval(ElementTransformation &T, const IntegrationPoint &ip)
-    {
-        double x[3];
-        Vector transip(x, 3);
-        T.Transform(ip, transip);
-        return (GFunction)? GFunction(_electrolyte_concentration.GetValue(T, ip), _electrode_surface_concentration, _electrode_max_concentration, transip) 
-            : TDGFunction(_electrolyte_concentration.GetValue(T, ip), _electrode_surface_concentration, _electrode_max_concentration, transip, GetTime() );
+        return (GFunction)? GFunction(_electrolyte_potential.GetValue(T, ip), _electrode_potential.GetValue(T, ip), _electrolyte_concentration.GetValue(T, ip), _electrode_surface_concentration, transip) 
+            : TDGFunction(_electrolyte_potential.GetValue(T, ip), _electrode_potential.GetValue(T, ip), _electrolyte_concentration.GetValue(T, ip), _electrode_surface_concentration, transip, GetTime() );
     }
--- utilis.hpp
+++ utilis.hpp
@@ -3,39 +3,40 @@
 using namespace std;
 using namespace mfem;
 
-double  function3(const double & electrolyte_potential, const double & electrode_potential, const Vector & x);
+using Args = double(const double &, const double &, const double &, const double &, const Vector &);
+using Func = function<Args>;
+using ArgsT = double(const double &, const double &, const double &, const double &, const Vector &, const double);
+using FuncT = function<ArgsT>;
+
+double  FluxJExt(const double & ce, const double & cs);
+double  FluxJ(const double & electrolyte_potential, const double & electrode_potential, const double & electrolyte_concentration, const double & electrode_surface_concentration, const Vector & x);
 
 class FluxJGridFuncCoefficient : public Coefficient
  {
     const GridFunction & _electrolyte_potential;
     const GridFunction & _electrode_potential;
-    function<double(const double &, const double &, const Vector &)>                GFunction;
-    function<double(const double &, const double &, const Vector &, const double)>  TDGFunction;
- public:
-    FluxJGridFuncCoefficient(const GridFunction & electrolyte_potential, const GridFunction & electrode_potential
-                       , function<double(const double &, const double &, const Vector &)> foo);
- 
-    FluxJGridFuncCoefficient(const GridFunction & electrolyte_potential, const GridFunction & electrode_potential
-                       , function<double(const double &, const double &, const Vector &, const double)> foo);
- 
-    virtual   double Eval(ElementTransformation &T, const IntegrationPoint &ip);
- };
-
-
- class FluxJExtGridFuncCoefficient : public Coefficient
- {
     const GridFunction & _electrolyte_concentration;
     const double & _electrode_surface_concentration;
-    const double & _electrode_max_concentration;
-    function<double(const double &, const double &, const double &, const Vector &)>                GFunction;
-    function<double(const double &, const double &, const double &, const Vector &, const double)>  TDGFunction;
+    Func               GFunction;
+    FuncT              TDGFunction;
  public:
-    FluxJExtGridFuncCoefficient(const GridFunction & electrolyte_concentration, const double & electrode_surface_concentration,
-        const double & electrode_max_concentration, function<double(const double &, const double &, const double &, const Vector &)> foo);
+    FluxJGridFuncCoefficient(
+    const GridFunction & electrolyte_potential, 
+    const GridFunction & electrode_potential,
+    const GridFunction & electrolyte_concentration,
+    const double & _electrode_surface_concentration, 
+    Func foo);
  
-    FluxJExtGridFuncCoefficient(GridFunction & electrolyte_concentration, const double & electrode_surface_concentration,
-        const double & electrode_max_concentration, function<double(const double &, const double &, const double &, const Vector &, const double)> foo);
+    FluxJGridFuncCoefficient(
+    const GridFunction & electrolyte_potential, 
+    const GridFunction & electrode_potential,
+    const GridFunction & electrolyte_concentration, 
+    const double & _electrode_surface_concentration, 
+    FuncT foo);
  
     virtual   double Eval(ElementTransformation &T, const IntegrationPoint &ip);
  };
+
+
+
  
