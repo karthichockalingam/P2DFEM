1b293e851805fd55bbff4b3110ffd7dd3aef0302
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2025-05-21 09:33:30 +0100

    Add ElectrodePotential

---
+++ ElectrodePotential.cpp
@@ -0,0 +1,33 @@
+
+#include "ElectrodePotential.hpp"
+#include "utilis.hpp"
+
+void ElectrodePotential::update(const BlockVector &u, GridFunction &ce, Coefficient &j)
+{
+   ParGridFunction u_gf(&fespace);
+   u_gf.SetFromTrueDofs(u);
+
+   real_t a = 1;
+
+   ConstantCoefficient kappa(1.0);
+   ProductCoefficient source(a, j);
+
+   IntegrationRule ir = IntRules.Get(Geometry::SEGMENT, 6);
+
+   delete K;
+   K = new ParBilinearForm(&fespace);
+   K->AddDomainIntegrator(new DiffusionIntegrator(kappa, &ir));
+   K->Assemble(0); // keep sparsity pattern of M and K the same
+   K->FormSystemMatrix(ess_tdof_list, Kmat);
+
+   delete Q;
+   Q = new ParLinearForm(&fespace);
+   Q->AddDomainIntegrator(new DomainLFIntegrator(source));
+   Q->Assemble();
+   Qvec = std::move(*(Q->ParallelAssemble()));
+   Qvec.SetSubVector(ess_tdof_list, 0.0); // do we need this?
+   
+   Kmat.Mult(u.GetBlock(block_id), z);
+   z.Neg();
+   z += Qvec;
+}
