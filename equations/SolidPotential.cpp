9b8ecfc749dec61681c4b9ba99a6671e7895bfea
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2026-01-13 16:47:20 +0000

    Update SolidPotential w/ correct coeffs/signs/scales

--- equations/SolidPotential.cpp
+++ equations/SolidPotential.cpp
@@ -3,14 +3,27 @@
 
 void SolidPotential::Update(const BlockVector &x, const Coefficient &j, const real_t &dt)
 {
-   ConstantCoefficient sigma(1.0);
-   FunctionCoefficient dummy([](const Vector & x){ return sin(2*M_PI*x(0)); });
-   ProductCoefficient source(dummy, const_cast<Coefficient&>(j));
+   // Source term.
+   Vector source_vec({
+      /* NE */ AN /* length scaling */ * (LNE / NNE * NX),
+      /* SEP */ 0.,
+      /* PE */ AP /* length scaling */ * (LPE / NPE * NX)});
+
+   PWConstCoefficient source_part(source_vec);
+   ProductCoefficient source(source_part, const_cast<Coefficient&>(j));
+
+   // Effective conductivity (does not account for electrode filler).
+   Vector sigma_vec({
+      /* NE */ (1 - EPS_N) * SIGN /* length scaling */ / (LNE / NNE * NX),
+      /* SEP */ 0.,
+      /* PE */ (1 - EPS_P) * SIGP /* length scaling */ / (LPE / NPE * NX)});
+
+   PWConstCoefficient sigma(sigma_vec);
 
    delete K;
    K = new ParBilinearForm(&fespace);
    K->AddDomainIntegrator(new DiffusionIntegrator(sigma));
-   K->Assemble(0); // keep sparsity pattern of M and K the same
+   K->Assemble();
    K->FormSystemMatrix(ess_tdof_list, Kmat);
 
    delete Q;
@@ -18,10 +31,9 @@ void SolidPotential::Update(const BlockVector &x, const Coefficient &j, const re
    Q->AddDomainIntegrator(new DomainLFIntegrator(source));
    Q->Assemble();
    Qvec = std::move(*(Q->ParallelAssemble()));
-   Qvec.SetSubVector(ess_tdof_list, 0.0); // do we need this?
+   Qvec.SetSubVector(ess_tdof_list, 0.0);
 
    Kmat.Mult(x.GetBlock(SP), b);
-   b.Neg();
    b += Qvec;
-   b *= 1./dt;
+   b *= -1./dt;
 }
