b8702a15c6919c26e804b868bbca9f46cc6f3277
Olha I. Yaman <Olha.Ivanyshyn-Yaman@stfc.ac.uk>
2026-02-11 18:21:11 +0000

    Update details.md and add some comments

--- cells/LGM50.hpp
+++ cells/LGM50.hpp
@@ -6,18 +6,18 @@ using namespace mfem;
 namespace LGM50 {
     // Negative electrode.
     const real_t cn0 = 29866.; // Initial concentration, negative electrode, mol/m^3
-    const real_t cnmax = 33133; // Maximum concentration, negative electrode, mol/m
+    const real_t cnmax = 33133; // Maximum concentration, negative electrode, mol/m^3
 
     const real_t rn = 5.86e-6; // Negative particle radius (m).
     const real_t negative_electrode_thickness = 85.2e-6;
 
-    const real_t eps_n = 0.25; // Porosity???
+    const real_t eps_n = 0.25; // Negative electrode electrolyte volume fraction
     const real_t eps_n_fi = 0.0;
     const real_t eps_n_s = 1 - eps_n - eps_n_fi;
 
-    const real_t kn_dim = 6.48e-7;
-    const real_t Dn = 3.3e-14; // Diffusion coefficient, negative electrode
-    const real_t An = 3 * eps_n_s / rn; // Negative electrode area (m^2).
+    const real_t kn_dim = 6.48e-7; //Negative electrode reaction rate, A m^{2.5}/mol^{1.5}
+    const real_t Dn = 3.3e-14; // Diffusion coefficient, negative electrode, m^2/s
+    const real_t An = 3 * eps_n_s / rn; // Negative electrode area (m^{-1}).
 
     const real_t sig_n = 215.; // Negative electrode conductivity (S/m).
 
@@ -34,12 +34,12 @@ namespace LGM50 {
     const real_t rp = 5.22e-6; // Positive particle radius (m).
     const real_t positive_electrode_thickness = 75.6e-6;
 
-    const real_t eps_p = 0.335; // Porosity???
+    const real_t eps_p = 0.335; // Positive electrode electrolyte volume fraction
     const real_t eps_p_fi = 0.0;
     const real_t eps_p_s = 1 - eps_p - eps_p_fi;
 
-    const real_t kp_dim = 3.42e-6;
-    const real_t Dp = 4.0e-15; // Diffusion coefficient, positive electrode
+    const real_t kp_dim = 3.42e-6; // Positive electrode reaction rate, A m^{2.5}/mol^{1.5}
+    const real_t Dp = 4.0e-15; // Diffusion coefficient, positive electrode, m^2/s
     const real_t Ap = 3 * eps_p_s / rp; // Positive electrode area (m^2).
 
     const real_t sig_p = 0.18; // Positive electrode conductivity (S/m).
@@ -56,12 +56,12 @@ namespace LGM50 {
     const real_t eps_s = 0.47;
 
     // Electrolyte.
-    const real_t ce0 = 1000.0;
-    real_t De(real_t ce)
+    const real_t ce0 = 1000.0; //initial electrolyte concentration (mol/(m^3))
+    real_t De(real_t ce) //diffusivity of lithium ions in the electrolyte (m^2/s)
     {
         return 8.794e-11 * pow(ce / 1000, 2) - 3.972e-10 * (ce / 1000) + 4.862e-10;
     }
-    real_t kappa(real_t x)
+    real_t kappa(real_t x) // electronic conductivity (S/m)
     {
         return 0.1297 * pow(x / 1000, 3) - 2.51 * pow(x / 1000, 1.5) + 3.329 * (x / 1000);
     }
@@ -73,6 +73,7 @@ namespace LGM50 {
     const real_t cell_area = cell_width * cell_length * cell_no_layers;
 
     const real_t I_typ = 5.0;    // Or I1C in Jubat.
+                                //Reference total current, A
 
     const real_t brugg = 1.5; // Currently the same in each region, may need to vary later.
 }
